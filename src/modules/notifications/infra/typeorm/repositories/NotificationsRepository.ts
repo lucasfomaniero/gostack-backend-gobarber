64915cff116373f5cb8b6d72f66ac375b391c32a
Lucas Maniero <lucasfomaniero@gmail.com>
2021-02-08 10:14:09 -0300

    feat mongodb integration

---
+++ src/modules/notifications/infra/typeorm/repositories/NotificationsRepository.ts
@@ -0,0 +1,30 @@
+import { getMongoRepository, MongoRepository } from 'typeorm';
+
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+import INotificationsRepository from '@modules/notifications/infra/typeorm/repositories/INotificationsRepository';
+
+import Notification from '../schemas/Notification';
+
+class NotificationsRepository implements INotificationsRepository {
+  private ormRepository: MongoRepository<Notification>;
+
+  constructor() {
+    this.ormRepository = getMongoRepository(Notification, 'mongo');
+  }
+
+  public async create({
+    recipientID,
+    content,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = this.ormRepository.create({
+      recipientID,
+      content,
+    });
+
+    await this.ormRepository.save(notification);
+
+    return notification;
+  }
+}
+
+export default NotificationsRepository;
